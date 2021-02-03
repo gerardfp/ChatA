a720b3aa40f8b55156050079c561298378cabedc
gerardfp <gerardfp@gmail.com>
2021-02-03 19:17:50 +0100

    Initial commit

---
+++ app/src/main/java/com/company/chata/SignInFragment.java
@@ -0,0 +1,31 @@
+package com.company.chata;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.company.chata.databinding.FragmentSignInBinding;
+
+
+public class SignInFragment extends Fragment {
+
+    private FragmentSignInBinding binding;
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return (binding = FragmentSignInBinding.inflate(inflater, container, false)).getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        
+    }
+}
