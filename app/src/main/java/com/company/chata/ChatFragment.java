02b7c245bf7dfaf291beadfb777602657b22a796
gerardfp <gerardfp@gmail.com>
2021-02-04 16:20:21 +0100

    --

--- app/src/main/java/com/company/chata/ChatFragment.java
+++ app/src/main/java/com/company/chata/ChatFragment.java
@@ -1,26 +1,22 @@
 package com.company.chata;
 
-import android.graphics.PostProcessor;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
-
-import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.company.chata.databinding.FragmentChatBinding;
-import com.company.chata.databinding.FragmentSignInBinding;
+import com.company.chata.databinding.ViewholderMensajeBinding;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.FirebaseFirestoreException;
-import com.google.firebase.firestore.QuerySnapshot;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -47,22 +43,68 @@ public class ChatFragment extends Fragment {
         mAuth = FirebaseAuth.getInstance();
         mDb = FirebaseFirestore.getInstance();
 
+        ChatAdapter chatAdapter = new ChatAdapter();
+        binding.chat.setAdapter(chatAdapter);
+
         binding.enviar.setOnClickListener(v -> {
             String texto = binding.mensaje.getText().toString();
             String fecha = LocalDateTime.now().toString();
             String autor = mAuth.getCurrentUser().getEmail();
 
             mDb.collection("mensajes").add(new Mensaje(texto, fecha, autor));
+
+            binding.mensaje.setText("");
         });
 
-        mDb.collection("mensajes").addSnapshotListener((value, error) -> {
+        mDb.collection("mensajes").orderBy("fecha").addSnapshotListener((value, error) -> {
             chat.clear();
             value.forEach(document -> {
                 chat.add(new Mensaje(
-                    document.getString("autor"),
+                    document.getString("mensaje"),
                     document.getString("fecha"),
-                    document.getString("mensaje")));
+                    document.getString("autor")));
             });
+
+            chatAdapter.notifyDataSetChanged();
+            binding.chat.scrollToPosition(chat.size()-1);
         });
+    }
+
+    class ChatAdapter extends RecyclerView.Adapter<MensajeViewHolder>{
+
+        @NonNull
+        @Override
+        public MensajeViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            return new MensajeViewHolder(ViewholderMensajeBinding.inflate(getLayoutInflater(), parent, false));
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull MensajeViewHolder holder, int position) {
+
+            Mensaje mensaje = chat.get(position);
+
+            holder.binding.nombre.setText(mensaje.autor);
+            holder.binding.mensaje.setText(mensaje.mensaje);
+            holder.binding.fecha.setText(mensaje.fecha);
+
+            if(mensaje.autor.equals(mAuth.getCurrentUser().getEmail())){
+                holder.binding.root.setGravity(Gravity.END);
+            } else {
+                holder.binding.root.setGravity(Gravity.START);
+            }
+        }
+
+        @Override
+        public int getItemCount() {
+            return chat.size();
+        }
+    }
+
+    static class MensajeViewHolder extends RecyclerView.ViewHolder{
+        ViewholderMensajeBinding binding;
+        public MensajeViewHolder(@NonNull ViewholderMensajeBinding binding) {
+            super(binding.getRoot());
+            this.binding = binding;
+        }
     }
 }
